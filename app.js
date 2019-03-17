4854312d58f652ac576e8a07220c3a611e87d8ab
sogchs <sogchs@gmail.com>
2019-03-17 10:41:30 +0100

    set app.jss

--- app.js
+++ app.js
@@ -5,13 +5,22 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const passport = require('passport');
 
-const columnRoutes = require('./routes/columns.routes');
+// ROUTES
+const studentRoutes = require('./routes/student.routes');
+const calendarRoutes = require('./routes/teacher.routes');
+const classRoutes = require('./routes/class.routes');
+const chatRoutes = require('./routes/chat.routes');
+const columnRoutes = require('./routes/column.routes');
 const cardsRoutes = require('./routes/cards.routes');
+const authRoutes = require('./routes/auth.routes')
+
 
 require('./configs/db.config');
-
+const cors = require('./configs/cors.config');
 const session = require('./configs/session.config');
+require('./configs/passport.config');
 
 const app = express();
 
@@ -19,11 +28,19 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors);
 
 app.use(session);
+app.use(passport.initialize());
+app.use(passport.session());
 
+app.use('/student', studentRoutes);
+app.use('/calendar', calendarRoutes);
+app.use('/class', classRoutes);
+app.use('/chat', chatRoutes);
 app.use('/columns', columnRoutes);
 app.use('/cards', cardsRoutes);
+app.use('/', authRoutes)
 
 app.use((req, res, next) => {
   res.locals.session = req.user;
