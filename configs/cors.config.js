6ed6e5c9e660136c7f0f99144733bd209e55ea62
sogchs <sogchs@gmail.com>
2019-04-12 18:50:04 +0200

    cors config

--- configs/cors.config.js
+++ configs/cors.config.js
@@ -1,33 +1,9 @@
-// const createError = require('http-errors');
-// const cors = require('cors');
+const originsAllowed = process.env.CORS_ORIGINS || ['http://localhost:3000'];
 
-// const allowedOrigins = process.env.CORS_URLS ? process.env.CORS_URLS.split(',') : ['http://localhost:3000']
-
-// module.exports = cors({
-//   origin: (origin, next) => {
-//     const allowed = !origin || allowedOrigins.indexOf(origin) !== -1;
-//     if (allowed) {
-//       next(null, allowed);
-//     } else {
-//       next(createError(401, 'Not allowed by CORS'));
-//     }
-//   },
-//   credentials: true
-// });
-
-const cors = require('cors')
-const createError = require('http-errors');
-
-const allowedOrigins = [process.env.CORS_URLS,'http://localhost:3000']
-module.exports = cors({
-  origin: (origin, next) => {
-    console.log("Moi-origin: ", origin)
-    const isAllowed = !origin || allowedOrigins.some(o => o === origin);
-    if(isAllowed) {
-      next(null,isAllowed);
-    } else {
-      next(createError(401, "Not allowed by CORS"))
-    }
+module.exports = {
+  origin: function(origin, cb) {
+    const allowed = originsAllowed.indexOf(origin) !== -1;
+    cb(null, allowed);
   },
   credentials: true
-})
+}
